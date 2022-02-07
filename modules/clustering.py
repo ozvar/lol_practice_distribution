46dc8df3efb4fb9b85500536ea28c168bc024a83
ozvar <ov525@york.ac.uk>
2022-02-07 21:56:16 +0000

    removed rule_based_clustering, adjusted output dirs and other minor edits

--- modules/clustering.py
+++ modules/clustering.py
@@ -1,35 +1,21 @@
 import numpy as np
+import pymc3 as pm
 
 from sklearn.cluster import MiniBatchKMeans as mbk
 
 from tslearn.clustering import TimeSeriesKMeans as tskm
-from .visualization import visualize_auto_elbow
+from tslearn.preprocessing import TimeSeriesScalerMeanVariance as tsmv
+
+from hdbscan import HDBSCAN
+from umap import UMAP
+
+from tensorflow.keras.callbacks import EarlyStopping
+
+from visualization import visualize_auto_elbow
 
 
-def rule_based_clustering(df, feature='nth_day'):
-    """
-    """
-    df = df.sort_values(['account_id', 'nth_match'])
-    feature_values = df.groupby('account_id')[feature].agg('max').values
-    labels = []
-    for value in feature_values:
-
-        if value <= 25:
-            labels.append(0)
-        elif 76 <= value <= 90:
-            labels.append(1)
-        elif 136 <= value <= 150:
-            labels.append(2)
-        else:
-            labels.append(-1)
-
-    return np.array(labels)
-
-
-def auto_elbow(n_clusters, inertias, verbose=True,
-               save_path='results\\figures\\base'):
-    """
-    """
+def auto_elbow(n_clusters, inertias, n_matches, start_gap, end_gap, fig_dir, verbose=True):
+    """Calculate and return optimal k number of clusters based on passed inertias from n_clusters clusters"""
     y = (inertias[0], inertias[-1])
     x = (n_clusters[0], n_clusters[-1])
 
@@ -42,18 +28,22 @@ def auto_elbow(n_clusters, inertias, verbose=True,
     optimal_k = np.argmax([grad - i for grad, i in zip(grad_line, inertias)])
     optimal_k = n_clusters[optimal_k]
     if verbose:
-        print(f'Optimal K found at {optimal_k}')
+        print('Optimal k found at {}'.format(optimal_k))
         visualize_auto_elbow(
             n_clusters=n_clusters,
             inertias=inertias,
+            n_matches=n_matches,
+            start_gap=start_gap,
+            end_gap=end_gap,
+            fig_dir=fig_dir,
             grad_line=grad_line,
-            optimal_k=optimal_k,
-            save_path=save_path
+            optimal_k=optimal_k
         )
-    return optimal_k
+        
+    return optimal_k, grad_line
 
 
-def auto_k_means(X, min_k=2, max_k=15, save_path='results\\figures\\base',
+def auto_k_means(X, min_k=2, max_k=15, n_matches=100, start_gap=1, end_gap=5, fig_dir='figures',
                  **kwargs):
     """
     """
@@ -82,8 +72,11 @@ def auto_k_means(X, min_k=2, max_k=15, save_path='results\\figures\\base',
         n_clusters=[i for i in range(min_k, max_k)],
         inertias=inertias,
         verbose=True,
-        save_path=save_path
-    )
+        n_matches=n_matches,
+        start_gap=start_gap,
+        end_gap=end_gap,
+        fig_dir=fig_dir
+    )[0]
     if len(X.shape) == 3:
         clust = tskm(
             n_clusters=optimal_k,
